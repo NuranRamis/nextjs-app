3d1cf7b3af3388fec1555a4b8aaf4f6571c6e5ed
Nuran Ramis <nuranrms@gmail.com>
2025-05-16 22:16:59 +0200

    add Tailwind CSS Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 const Home = () => {
   return (
-    <h1 className="text-3xl text-violet-700 font-black">Welcome to Next.js</h1>
+    <div>
+    <h1 className="h1-bold">Welcome to Next.js</h1>
+    </div>
   );
 };
 
