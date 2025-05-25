9a21c236d3f1c1c9b7e1b43831acfd2b91ece6d5
Nuran Ramis <nuranrms@gmail.com>
2025-05-25 11:43:10 +0200

    Assets and Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono as GiestMono} from "next/font/google";
+import { Geist, Inter as GiestMono, Inter, Space_Grotesk as SpaceGrotesk} from "next/font/google";
 import "./globals.css";
+
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+const spaceGrotesk = SpaceGrotesk({
+  variable: "--font-space-grotesk",
+  subsets: ["latin"],
+});
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable}  ${geistMono.variable} ${geistSans.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 const Home = () => {
   return (
-    <div>
+    <>
     <h1 className="h1-bold">Welcome to Next.js</h1>
-    </div>
+    <h1 className="h1-bold font-inter">Welcome to Next.js (Inter)</h1>
+    <h1 className="h1-bold font-space-grotesk">Welcome to Next.js (Inter)</h1>
+    </>
   );
 };
 
